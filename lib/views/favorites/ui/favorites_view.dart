5867612a9e213f8b62551a82e04317d68f8180e5
amrElzohairy <aelzohairy9@gmail.com>
2025-04-09 15:35:26 +0200

    finish fav view

--- lib/views/favorites/ui/favorites_view.dart
+++ lib/views/favorites/ui/favorites_view.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:online_market/views/home/ui/widgets/products_list.dart';
 
 class FavoritesView extends StatelessWidget {
   const FavoritesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center( 
-      child: Text("Favorites View"),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          const SizedBox(height: 20),
+          const Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Your Favorite Products",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          const ProductsList(),
+        ],
+      ),
     );
   }
 }
